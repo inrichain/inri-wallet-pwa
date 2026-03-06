80a9d206efc8ec8ac2e184cb09916ea545c353b6
INRI CHAIN <contact@inri.life>
2026-03-06 12:54:35 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/inri-wallet-pwa/',
+
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'INRI Wallet',
+        short_name: 'INRI Wallet',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#0b0b0f',
+        theme_color: '#4e8cff',
+        icons: [
+          {
+            src: '/favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
